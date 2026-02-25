21949aadb7bacdcad448f2a892e622b7bf5343de
jihun <jihun315016@gmail.com>
2026-02-26 00:51:28 +0900

    [EmployeeAPP] - EF Core Package 설치 - Model 정의

---
+++ EmployeeAPP/EmployeeAPP/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeAPP.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; } = new();
+    }
+}
---
+++ EmployeeAPP/EmployeeAPP/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeAPP.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
